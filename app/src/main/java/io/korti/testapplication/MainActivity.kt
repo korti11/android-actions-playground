dbfaa8d689963052b94859f52359d1e25fa6d72b
Korti <lucas.korthauer@gmail.com>
2019-12-17 12:34:08 +0100

    Unlock git-crypt secrets for testing after checkout.

--- app/src/main/java/io/korti/testapplication/MainActivity.kt
+++ app/src/main/java/io/korti/testapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package io.korti.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(MainActivity::class.java.simpleName, getString(R.string.API_KEY))
     }
 }
